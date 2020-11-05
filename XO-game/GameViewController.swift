3b91ae2fe6cf41952a5de89fee9b9fd1684fd5c3
Sergei Morugin <cmorugin@gmail.com>
2020-11-05 18:24:34 +0300

    add code from lecture

--- XO-game/GameViewController.swift
+++ XO-game/GameViewController.swift
@@ -8,23 +8,59 @@
 
 import UIKit
 
-class GameViewController: UIViewController {
+public class GameViewController: UIViewController {
 
     @IBOutlet var gameboardView: GameboardView!
     @IBOutlet var firstPlayerTurnLabel: UILabel!
     @IBOutlet var secondPlayerTurnLabel: UILabel!
     @IBOutlet var winnerLabel: UILabel!
     @IBOutlet var restartButton: UIButton!
+    private lazy var referee = Referee(gameboard: self.gameboard)
     
-    override func viewDidLoad() {
+    private let gameboard = Gameboard()
+    private var currentState: GameState! {
+        didSet {
+            self.currentState.begin()
+        }
+    }
+    
+    public override func viewDidLoad() {
         super.viewDidLoad()
+        self.goToFirstState()
+        
         gameboardView.onSelectPosition = { [weak self] position in
             guard let self = self else { return }
-            self.gameboardView.placeMarkView(XView(), at: position)
+            self.currentState.addMark(at: position)
+            if self.currentState.isCompleted {
+                self.goToNextState()
+            }
+        }
+    }
+    
+    private func goToFirstState() {
+        self.currentState = PlayerInputState(player: .first,
+                                             gameViewController: self,
+                                             gameboard: gameboard,
+                                             gameboardView: gameboardView)
+    }
+
+    private func goToNextState() {
+        if let winner = self.referee.determineWinner() {
+            self.currentState = GameEndedState(winner: winner, gameViewController: self)
+            return
+        }
+        if let playerInputState = currentState as? PlayerInputState {
+            self.currentState = PlayerInputState(player: playerInputState.player.next,
+                                                 gameViewController: self,
+                                                 gameboard: gameboard,
+                                                 gameboardView: gameboardView)
         }
     }
     
     @IBAction func restartButtonTapped(_ sender: UIButton) {
+        self.goToFirstState()
+        gameboard.clear()
+        gameboardView.clear()
         
     }
 }
